9927ead6e16e78861f27f5f1c880cc9f797134b4
Obitus187 <88170059+Obitus187@users.noreply.github.com>
2022-06-15 17:09:17 -0600

    First Queries

---
+++ BigQuery_Covid.sql
@@ -0,0 +1,13 @@
+-- Aggregating data by Country and doing some calculations such as fully vaccinated divided by country's population
+
+SELECT country_name, sum(new_confirmed) as Confirmed, sum(new_deceased) as Deceased, sum(new_persons_fully_vaccinated) as Vaccinated, (sum(new_persons_fully_vaccinated)/max(population)) as Percent_Vaxed, max(population) as population, sum(new_confirmed_male) as male_confirmed, sum(new_deceased_male) as male_deceased, sum(new_confirmed_female) as female_confirmed, sum(new_deceased_female) as femaled_deceased, (sum(new_confirmed_male)/sum(new_confirmed)) as percent_male_confirmed, (sum(new_confirmed_female)/sum(new_confirmed)) as percent_female_confirmed
+FROM `bigquery-public-data.covid19_open_data.covid19_open_data` 
+group by country_name
+order by Percent_Vaxed desc
+LIMIT 1000
+
+-- Creating a table for daily data updates
+
+SELECT date, country_name, new_confirmed, new_deceased, cumulative_confirmed, cumulative_deceased, new_persons_vaccinated, cumulative_persons_vaccinated, new_persons_fully_vaccinated, cumulative_persons_fully_vaccinated, stringency_index, average_temperature_celsius, rainfall_mm, snowfall_mm
+FROM `bigquery-public-data.covid19_open_data.covid19_open_data` 
+order by date
